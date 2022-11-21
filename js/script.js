556599f87b38a6a303ae3a6e30fdf82b253e8513
Ross-Berry <93287406+Ross-Berry@users.noreply.github.com>
2022-11-21 16:41:31 +0000

    If applied, this commit will create playRound()

--- js/script.js
+++ js/script.js
@@ -3,3 +3,27 @@ function getComputerChoice() {
 
     return choices[Math.floor((Math.random() * 3))];
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You Lose! Scissors beats Paper";
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You Lose! Rock beats Scissors";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You Win! Scissors beats Paper";
+    } else {
+        return "It's a draw!";
+    }
+}
+
+playerSelection = "RocK";
+computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
