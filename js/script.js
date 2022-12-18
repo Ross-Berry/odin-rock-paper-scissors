7aec44eb5fb4ce716307805f4474efb4955ca213
Ross-Berry <93287406+Ross-Berry@users.noreply.github.com>
2022-12-18 19:07:00 +0000

    If applied, this commit will create two new functions
    The two new functions created are game() and calcWinner().

--- js/script.js
+++ js/script.js
@@ -5,25 +5,65 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
+    let playerLowerCase = playerSelection.toLowerCase()
 
-    if (playerSelection == "rock" && computerSelection == "paper") {
-        return "You Lose! Paper beats Rock";
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        return "You Win! Rock beats Scissors";
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-        return "You Win! Paper beats Rock";
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        return "You Lose! Scissors beats Paper";
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        return "You Lose! Rock beats Scissors";
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "You Win! Scissors beats Paper";
-    } else {
-        return "It's a draw!";
+    if (playerLowerCase === "rock" && computerSelection === "paper") {
+        return "You Lose!";
+    } else if (playerLowerCase === "rock" && computerSelection === "scissors") {
+        return "You Win!";
+    } else if (playerLowerCase === "rock" && computerSelection === "rock") {
+        return "Draw!";    
+    } else if (playerLowerCase === "paper" && computerSelection === "rock") {
+        return "You Win!";
+    } else if (playerLowerCase === "paper" && computerSelection === "scissors") {
+        return "You Lose!";
+    } else if (playerLowerCase === "paper" && computerSelection === "paper") {
+        return "Draw!"; 
+    } else if (playerLowerCase === "scissors" && computerSelection === "rock") {
+        return "You Lose!";
+    } else if (playerLowerCase === "scissors" && computerSelection === "paper") {
+        return "You Win!";
+    } else if (playerLowerCase === "scissors" && computerSelection === "scissors") {
+        return "Draw!"; 
     }
 }
 
-playerSelection = "RocK";
-computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 1; i <= 5; i++) {
+        playerSelection = prompt("Choose either 'rock,' 'paper,' or 'scissors'?", "")
+
+        switch(playRound(playerSelection, computerSelection)) {
+            case "You Win!":
+                console.log(`Player wins round ${i}`)
+                playerScore++
+            break;
+            case "You Lose!":
+                console.log(`Computer wins round ${i}`)
+                computerScore++
+            break;
+            case "Draw!":
+                console.log(`Round ${i} draw`)
+            break;
+        }
+    }
+    
+    calcWinner(playerScore, computerScore);
+}
+
+function calcWinner(playerScore, computerScore) {
+    if (playerScore > computerScore) {
+        console.log(`Player won by ${playerScore} to ${computerScore}`)
+    } else if (playerScore < computerScore) {
+        console.log(`Computer won by ${computerScore} to ${playerScore}`)
+    } else {
+        console.log(`It's a tie of ${playerScore} - ${computerScore}`)
+    }
+}
+
+let playerSelection = "";
+let computerSelection = getComputerChoice();
+
+game();
